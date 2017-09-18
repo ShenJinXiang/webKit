18a0119277e79a8283fbf41c32fb158b0e894e6c
shenjinxiang <sjx-sword@163.com>
2017-09-19 00:06:10 +0800

    layui

--- src/com/qysoft/platform/actions/FwhAction.java
+++ src/com/qysoft/platform/actions/FwhAction.java
@@ -1,11 +1,8 @@
 package com.qysoft.platform.actions;
 
-import com.jfinal.aop.Before;
-import com.jfinal.plugin.activerecord.Record;
 import com.qysoft.platform.services.FwhService;
 import com.qysoft.rapid.actions.RapidAction;
 import com.qysoft.rapid.aop.annotation.AutoService;
-import com.qysoft.rapid.aop.interceptor.JsonResultInterceptor;
 
 /**
  * Created by shenjinxiang on 2017/9/11.
@@ -15,10 +12,8 @@ public class FwhAction extends RapidAction {
     @AutoService
     private FwhService fwhService;
 
-    @Before(JsonResultInterceptor.class)
     public void index() {
-        String id = getPara("id");
-        Record record = fwhService.getFwuById(id);
-        setJson(record);
+
+        renderJsp("/WEB-INF/pages/fwh.jsp");
     }
 }
